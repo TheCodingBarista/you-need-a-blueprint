71118e74dc597c202c602a756b6077ed78572414
melissa_holt <melissa.holt21@gmail.com>
2021-01-09 15:40:25 -0800

    Configure oauth/devise, add users

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   resources :budgets do
     resources :categories
   end
+  root 'categories#index'
 end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_09_214920) do
+ActiveRecord::Schema.define(version: 2021_01_09_230846) do
 
   create_table "budgets", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,20 @@ ActiveRecord::Schema.define(version: 2021_01_09_214920) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["budget_id"], name: "index_categories_on_budget_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "provider"
+    t.string "uid"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
